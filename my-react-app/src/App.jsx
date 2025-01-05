efb3b5b6fe13102cf3fca293c700eeb01ec5d8d3
Mudit <goyalmudit82@gmail.com>
2025-01-05 22:35:16 +0530

    footer

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,13 +4,16 @@ import Header from "./components/Header";
 
 // import TrackingProgress from "./components/TrackingProgress";
 import ProvidersList from "./components/ProvidersList";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       {/* <TrackingProgress /> */}
+
       <ProvidersList />
+      <Footer />
     </div>
   );
 };
